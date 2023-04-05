499ec49f2828abcd699e39d014179833fc7f4b9a
joelcastagnani <joelcastagnani@hotmail.com>
2023-04-05 20:02:06 -0300

    termine el punto 2

--- src/clases/Colegio.java
+++ src/clases/Colegio.java
@@ -1,19 +1,50 @@
 package clases;
 
+import java.util.ArrayList;
+
 public class Colegio {
     //region Atributos
-
+    ArrayList<Persona> miembrosColegio;
     //endregion
 
     //region Contructores
-
-//endregion
-
-    //region Getters y setters
-
+    public Colegio() {
+        this.miembrosColegio = new ArrayList<Persona>();
+    }
     //endregion
 
     //region Metodos
+    public void addPersona (Persona personita){
+        miembrosColegio.add(personita);
+    }
+    public int getCantEstudiantes (){
+        int cant = 0;
 
+        for (Persona miembro : miembrosColegio){
+            if (miembro instanceof Estudiante)
+                cant++;
+        }
+        return cant;
+    }
+    public int getCantStaff(){
+        int cant = 0;
+
+        for (Persona miembro : miembrosColegio){
+            if (miembro instanceof MiembroStaff)
+                cant++;
+        }
+        return cant;
+    }
+    public double getIngresos (){
+        double ingresos = 0;
+
+        for (Persona miembro : miembrosColegio){
+            if (miembro instanceof Estudiante)
+                ingresos += ((Estudiante) miembro).cuotaMensual;
+        }
+        return ingresos;
+    }
     //endregion
+
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,38 @@
 package com.company;
 
+import clases.Colegio;
 import clases.Estudiante;
+import clases.MiembroStaff;
 
 public class Main {
 
     public static void main(String[] args) {
-        //aca
 
+        //region PUNTO 2
+        Estudiante uno = new Estudiante("38831258", "uno", "castagnani", "jcastagnani@gmail.com", "dsa", "2021", 5000, "programacion");
+        Estudiante dos = new Estudiante("38831258", "dos", "castagnani", "jcastagnani@gmail.com", "dsa", "2021", 5000, "programacion");
+        Estudiante tres = new Estudiante("38831258", "tres", "castagnani", "jcastagnani@gmail.com", "dsa", "2021", 5000, "programacion");
+        Estudiante cuatro = new Estudiante("38831258", "cuatro", "castagnani", "jcastagnani@gmail.com", "dsa", "2021", 5000, "programacion");
+
+        MiembroStaff m1 = new MiembroStaff("12654985", "m1", "dsa", "dsad", "vras", 15000, "manana");
+        MiembroStaff m2 = new MiembroStaff("12654985", "m2", "dsa", "dsad", "vras", 15000, "manana");
+        MiembroStaff m3 = new MiembroStaff("12654985", "m3", "dsa", "dsad", "vras", 15000, "manana");
+        MiembroStaff m4 = new MiembroStaff("12654985", "m4", "dsa", "dsad", "vras", 15000, "manana");
+
+        Colegio colegio = new Colegio();
+        colegio.addPersona(uno);
+        colegio.addPersona(dos);
+        colegio.addPersona(tres);
+        colegio.addPersona(cuatro);
+        colegio.addPersona(m1);
+        colegio.addPersona(m2);
+        colegio.addPersona(m3);
+        colegio.addPersona(m4);
+
+        System.out.println("La cantidad de estudiantes es: " + colegio.getCantEstudiantes());
+        System.out.println("La cantidad de Staff es: " + colegio.getCantStaff());
+        System.out.println("Ingresos: " + colegio.getIngresos());
+        //endregion
+        
     }
 }
